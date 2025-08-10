4aada9a1ffcc669f2e5ff059657cda4b8dd35ed8
Boopathi C <118243121+BoopathiC@users.noreply.github.com>
2025-08-10 22:11:36 +0530

    Week 8

--- Week8_Git/Week-8/GIT/1. Git-HOL/commands.sh
+++ Week8_Git/Week-8/GIT/1. Git-HOL/commands.sh
@@ -6,3 +6,4 @@ git commit -m "Initial commit with welcome.txt"
 # Replace <your-repo-url> with your actual GitHub/GitLab repo URL
 # git remote add origin <your-repo-url>
 # git push -u origin master
+
